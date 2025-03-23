e3d87cfc7ab317d8b85890f3ea916ecaa3c7d115
luoxbt <luoxbt@gmail.com>
2025-03-23 19:57:57 +0800

    Add files via upload

---
+++ tokenAddresses.js
@@ -0,0 +1,22 @@
+/**
+ * 代币合约地址列表
+ * 这个文件包含了所有需要在LUMINDEX中显示的代币合约地址
+ */
+
+export const tokenAddresses = [
+    '0x1bc0c42215582d5A085795f4baDbaC3ff36d1Bcb',
+    '0x22aF33FE49fD1Fa80c7149773dDe5890D3c76F3b',
+    '0x20DD04c17AFD5c9a8b3f2cdacaa8Ee7907385BEF',
+    '0x06f71fb90F84b35302d132322A3C90E4477333b0',
+    '0x36B0Bc9B7cd78C589076c8Fb7CD3f68b25b9770a',
+    '0x1d008f50FB828eF9DEbBEaE1B71FfFe929bf317',
+    '6p6xgHyF7AeE6TZkSmFsko444wqoP15icUSqi2jfGiPN',
+    '0x5C85D6C6825aB4032337F11Ee92a72DF936b46F6',
+    '0xe920d1DA9A4D59126dC35996Ea242d60EFca1304',
+    '0xffff003a6bad9b743d658048742935fffe2b6ed7',
+    '0x0c880f6761f1af8d9aa9c466984b80dab9a8c9e8',
+    '0xb2f776e9c1c926c4b2e54182fac058da9af0b6a5'
+];
+
+// 您可以在此文件中添加更多代币地址，或者修改现有地址
+// 修改此文件后，只需刷新页面即可看到更新
